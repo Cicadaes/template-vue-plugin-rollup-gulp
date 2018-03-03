6226e0bb5c63e4c42e68df486cfbb31ae12858da
Cicadaes <cicadaes@sina.com>
2018-03-03 07:36:20 -0800

    Add version and author

--- metadata.js
+++ metadata.js
@@ -1,22 +1,25 @@
 module.exports = {
     prompts: {
         name: {
-            when: 'isNotTest',
             type: 'string',
             required: true,
             message: 'Plugin name',
         },
         description: {
-            when: 'isNotTest',
             type: 'string',
             required: false,
             message: 'Plugin description',
             default: 'A Vue.js Plugin',
         },
         author: {
-            when: 'isNotTest',
             type: 'string',
             message: 'Author',
+        },
+        version: {
+            type: 'string',
+            required: false,
+            message: 'Plugin version',
+            default: '1.0.0',
         }
     },
 }
